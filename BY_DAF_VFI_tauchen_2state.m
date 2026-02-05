6b6cb569330449289821ecc36ae92b881e018bc4
diegoaf00 <diegoaf00@gmail.com>
2026-02-05 22:30:28 +0000

    VFi two states IRFs

--- BY_DAF_VFI_tauchen_2state.m
+++ BY_DAF_VFI_tauchen_2state.m
@@ -67,6 +67,12 @@ for j=1:Ns2
 end
 %% VFI: POLICY FUNCTIONS
 
+% I have j as current sigma^2, i as current x. 
+% For that i, there are transition probabilities that sum up to one defined on the j grid of x. 
+% If I want to take expectations over sigma^2 and x at t+1, the transition probability matrix of sigma gives me the probability of ending up at each k for sigma^2 in t+1. 
+% If k != j, the grid for x changes to k, but I have transition probabilities for x defined over j. 
+% Hence, I have to use those probabilities. 
+% Since the k grid is different, I interpolate the value of the value function to match grid j.
 function [V1_VC] = value_function_iteration_VC2(Nx,beta,gamma,theta,mu,x_grid,sigma2_grid,P_matrix,max_iter,crit,Ns2,S_matrix)
     V0=ones(Nx,Ns2); % Initial value function
     V1_VC=ones(Nx,Ns2); % Initial value function after iteration i
@@ -192,3 +198,86 @@ xlabel('x_t'); ylabel('\sigma_t^2');zlabel('r_f')
 grid on
 title("Log risk-free rate")
 saveas(figure(8),'figure8_two.png')
+
+%% VFI: IRFs
+function [x,s2,diff_z,diff_zm] = function2_1_vfi1_irf_z_zm(T,x0,sigma20,rho,phi_e,nu1,sigma,sigma_w, ...
+                        x_grid,sigma2_grid,V1_PC,V1_PD,shockType)
+    shockSize=1;
+    x=zeros(T+1,1);
+    s2=zeros(T+1,1);
+    x(1)=x0;
+    s2(1)=sigma20;
+    eps=zeros(T,1);
+    omg=zeros(T,1);
+
+    if strcmpi(shockType,'epsilon')
+        eps(1)=shockSize;
+    elseif strcmpi(shockType,'omega')
+        omg(1)=shockSize;
+    else
+        error('shockType must be ''epsilon'' or ''omega');
+    end
+
+    for t=1:T
+        x(t+1)=rho*x(t)+phi_e*sqrt(s2(t))*eps(t);
+        s2(t+1)=sigma^2+nu1*(s2(t)-sigma^2)+sigma_w*omg(t);
+        s2(t+1)=max(s2(t+1),0);
+    end
+    
+    z=zeros(T+1,1); % Pre-define z
+    zm=zeros(T+1,1); % Pre-define zm
+    for t=1:T+1
+        sigma2_winsor=min(max(s2(t),min(sigma2_grid)),max(sigma2_grid)); % I make sure that sigma^2_t is bewteen sigma^2_max and sigma^2_min from the Tauchen discretization
+        PC_over_sigma=zeros(size(sigma2_grid,1),1);
+        PD_over_sigma=zeros(size(sigma2_grid,1),1); 
+        for j=1:size(sigma2_grid,1)
+            x_winsor=min(max(x(t),min(x_grid{j})),max(x_grid{j})); % I make sure that x is bewteen x_max and x_min from the Tauchen discretization for each point in the sigma^2 grid
+            PC_over_sigma(j)=interp1(x_grid{j},V1_PC(:,j),x_winsor); % I interpolate the value of the PC ratio over x given j
+            PD_over_sigma(j)=interp1(x_grid{j},V1_PD(:,j),x_winsor); % I interpolate the value of the PD ratio over x given j
+        end
+        exp_z=interp1(sigma2_grid,PC_over_sigma,sigma2_winsor); % I interpolate the value of the PC ratio over sigma^2
+        exp_zm=interp1(sigma2_grid,PD_over_sigma,sigma2_winsor); % I interpolate the value of the PD ratio over sigma^2
+        z(t)=log(exp_z);
+        zm(t)=log(exp_zm);
+    end   
+
+    sigma2_winsor_ss=min(max(sigma20,min(sigma2_grid)),max(sigma2_grid)); % I make sure that sigma^2_ss is bewteen sigma^2_max and sigma^2_min from the Tauchen discretization
+    PC_over_sigma_ss=zeros(size(sigma2_grid,1),1);
+    PD_over_sigma_ss=zeros(size(sigma2_grid,1),1); 
+    for j=1:size(sigma2_grid,1)
+        x_winsor_ss=min(max(x0,min(x_grid{j})),max(x_grid{j})); % I make sure that x is bewteen x_max and x_min from the Tauchen discretization for each point in the sigma^2 grid
+        PC_over_sigma_ss(j)=interp1(x_grid{j},V1_PC(:,j),x_winsor_ss); % I interpolate the value of the PC ratio over x given jss
+        PD_over_sigma_ss(j)=interp1(x_grid{j},V1_PD(:,j),x_winsor_ss); % I interpolate the value of the PD ratio over x given jss
+    end
+    exp_z_ss=interp1(sigma2_grid,PC_over_sigma_ss,sigma2_winsor_ss); % I interpolate the value of the PC ratio over sigma^2
+    exp_zm_ss=interp1(sigma2_grid,PD_over_sigma_ss,sigma2_winsor_ss); % I interpolate the value of the PD ratio over sigma^2
+    z_ss=log(exp_z_ss);
+    zm_ss=log(exp_zm_ss);
+    diff_z=z-z_ss;
+    diff_zm=zm-zm_ss;
+end
+
+T=250;
+t=(1:T)';
+x_ss=0; % Unconditional expectation of x_t
+sigma2_ss=sigma^2; % Unconditional expectation of sigma_t^2
+
+[x_eps,s2_eps,diff_z_eps,diff_zm_eps] = function2_1_vfi1_irf_z_zm(T,x_ss,sigma2_ss,rho,phi_e,nu1,sigma,sigma_w, ...
+                   x_grid,sigma2_grid,V1_PC,V1_PD,"epsilon");
+
+[x_omega,s2_omega,diff_z_omega,diff_zm_omega] = function2_1_vfi1_irf_z_zm(T,x_ss,sigma2_ss,rho,phi_e,nu1,sigma,sigma_w, ...
+                    x_grid,sigma2_grid,V1_PC,V1_PD,"omega");
+
+figure(9)
+plot(t,diff_z_eps(2:end),t,diff_zm_eps(2:end),'LineWidth',2);
+grid on;xlabel('t');ylabel('Difference w.r.t. ss');
+legend('\Delta z_t (epsilon shock)','\Delta z_{m,t} (epsilon shock)','Location','Best');
+title('IRFs to an \epsilon shock');
+saveas(figure(9),'figure9_two.png')
+
+figure(10)
+plot(t,diff_z_omega(2:end),t,diff_zm_omega(2:end),'LineWidth',2);
+grid on;xlabel('t');ylabel('Difference w.r.t. ss');
+legend('\Delta z_t (omega shock)','\Delta z_{m,t} (omega shock)','Location','Best');
+title('IRFs to an \omega shock');
+saveas(figure(10),'figure10_two.png')
