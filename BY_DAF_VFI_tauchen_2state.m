d2e6c1d315ff397629c12a512225a0dc60c78c4e
diegoaf00 <diegoaf00@gmail.com>
2026-02-06 00:44:44 +0000

    Full VFI Tauchen

--- BY_DAF_VFI_tauchen_2state.m
+++ BY_DAF_VFI_tauchen_2state.m
@@ -88,8 +88,6 @@ function [V1_VC] = value_function_iteration_VC2(Nx,beta,gamma,theta,mu,x_grid,si
             V1_VC(:,j)=((1-beta)+beta.*exp(((1-gamma)/theta).*(mu+x_grid{j})+(0.5/theta)*(1-gamma)^2*sigma2_grid(j)).*...
                 ((P_matrix{j}*V_interpolate)*(S_matrix(j,:)') ).^(1/theta)).^(theta/(1-gamma)); % See pdf
         end
-        max(abs(V1_VC(:)-V0(:)))
-        it
         % Check for convergence
         if max(abs(V1_VC(:)-V0(:)))<crit
             terminate=1;
@@ -112,9 +110,7 @@ function [V1_PC] = value_function_iteration_PC2(Nx,beta,psi,theta,mu,x_grid,sigm
                 V_interpolate(:,k)=interp1(x_grid{k},(1+V0(:,k)).^theta,x_winsor); % Interpolate V for the expectation
             end
             V1_PC(:,j)=beta.*exp((1-1/psi).*(mu+x_grid{j})+0.5*theta*(1-1/psi)^2*sigma2_grid(j)).*((P_matrix{j}*V_interpolate)*(S_matrix(j,:)')).^(1/theta); % See pdf 
-        end
-        max(abs(V1_PC(:)-V0(:)))
-        it
+         end
         % Check for convergence
         if max(abs(V1_PC(:)-V0(:)))<crit
             terminate=1;
@@ -139,8 +135,6 @@ function [V1_PD] = value_function_iteration_PD2(Nx,beta,psi,theta,mu,x_grid,sigm
             V1_PD(:,j)=beta^theta.*exp((theta-1-theta/psi).*(mu+x_grid{j})+mu_d+phi*x_grid{j}+0.5*(theta-1-theta/psi)^2*sigma2_grid(j)+0.5*phi_d^2*sigma2_grid(j)).* ...
                 (PC(:,j)).^(1-theta).*(P_matrix{j}*V_interpolate)*(S_matrix(j,:)'); % See pdf 
         end
-        max(abs(V1_PD(:)-V0(:)))
-        it
         % Check for convergence
         if max(abs(V1_PD(:)-V0(:)))<crit
             terminate=1;
@@ -281,3 +275,161 @@ grid on;xlabel('t');ylabel('Difference w.r.t. ss');
 legend('\Delta z_t (omega shock)','\Delta z_{m,t} (omega shock)','Location','Best');
 title('IRFs to an \omega shock');
 saveas(figure(10),'figure10_two.png')
+
+%% VFI: TABLE 5 POHL ET AL. (2018)
+
+% POPULATION
+function [expected_excess,expected_Rf,sigma_Rm,sigma_Rf,sigma_pd,expected_pd] = function2_simulate_vfi_tauchen_table2(...
+    T,burn,rho,phi_e,sigma,mu,mu_d,phi,phi_d,beta,psi,theta,x_grid,V1_PC,V1_PD,P_matrix,nu1,sigma_w,sigma2_grid,S_matrix,Nx,Ns2)
+    
+    x=zeros(T,1);
+    s2=zeros(T,1);
+    g=zeros(T,1);
+    gd=zeros(T,1);
+    rm=zeros(T,1);
+
+    epsilon=randn(T,1);
+    eta=randn(T,1);
+    u=randn(T,1);
+    omega=randn(T,1);
+
+    x(1)=0; % Unconditional expectation of x_t
+    s2(1)=sigma^2; % Unconditional expectation of sigma_t^2
+
+    for t=1:T-1
+        x(t+1)=rho*x(t)+phi_e*sqrt(s2(t))*epsilon(t+1);
+        g(t+1)=mu+x(t)+sqrt(s2(t))*eta(t+1);
+        gd(t+1)=mu_d+phi*x(t)+phi_d*sqrt(s2(t))*u(t+1);
+        s2(t+1)=sigma^2+nu1*(s2(t)-sigma^2)+sigma_w*omega(t+1);
+        s2(t+1)=max(s2(t+1),1e-9);
+    end
+
+    z=zeros(T,1); % Pre-define z
+    zm=zeros(T,1); % Pre-define zm
+    exp_zm=zeros(T,1); % Pre-define exp(zm)
+    [Rf]=function2_Rf(Nx,beta,mu,psi,theta,x_grid,sigma2_grid,P_matrix,V1_PC,Ns2,S_matrix); % Get Rf
+    Rf_interpol=zeros(T,1); % Pre-define Rf (with interpolation)
+    for t=1:T
+        sigma2_winsor=min(max(s2(t),min(sigma2_grid)),max(sigma2_grid)); % I make sure that sigma^2_t is bewteen sigma^2_max and sigma^2_min from the Tauchen discretization
+        PC_over_sigma=zeros(size(sigma2_grid,1),1);
+        PD_over_sigma=zeros(size(sigma2_grid,1),1); 
+        Rf_over_sigma=zeros(size(sigma2_grid,1),1);
+        for j=1:size(sigma2_grid,1)
+            x_winsor=min(max(x(t),min(x_grid{j})),max(x_grid{j})); % I make sure that x is bewteen x_max and x_min from the Tauchen discretization for each point in the sigma^2 grid
+            PC_over_sigma(j)=interp1(x_grid{j},V1_PC(:,j),x_winsor); % I interpolate the value of the PC ratio over x given j
+            PD_over_sigma(j)=interp1(x_grid{j},V1_PD(:,j),x_winsor); % I interpolate the value of the PD ratio over x given j
+            Rf_over_sigma(j)=interp1(x_grid{j},Rf(:,j),x_winsor); % I interpolate the value of Rf over x given j
+        end
+        exp_z=interp1(sigma2_grid,PC_over_sigma,sigma2_winsor); % I interpolate the value of the PC ratio over sigma^2
+        exp_zm(t)=interp1(sigma2_grid,PD_over_sigma,sigma2_winsor); % I interpolate the value of the PD ratio over sigma^2
+        Rf_interpol(t)=interp1(sigma2_grid,Rf_over_sigma,sigma2_winsor); % I interpolate the value of Rf over sigma^2
+        z(t)=log(exp_z);
+        zm(t)=log(exp_zm(t));
+    end
+    rf=log(1+Rf_interpol);
+
+    for t=1:T-1
+        rm(t+1)=log(exp_zm(t+1) + 1)-log(exp_zm(t))+gd(t+1); % log market return
+    end
+
+    t=(burn+1):(T-1);
+
+    rm_sim=rm(t);
+    rf_sim=rf(t);
+    zm_sim=zm(t);
+    gd_sim=gd(t);
+
+    T_months=length(rm_sim);
+    years=floor(T_months/12); % Number of full years
+    t_years=1:(12*years);
+
+    rm_reshape=reshape(rm_sim(t_years),12,years);
+    rf_reshape=reshape(rf_sim(t_years),12,years);
+    gd_reshape=reshape(gd_sim(t_years),12,years);
+
+    rm_annual=sum(rm_reshape,1)'; % Get annual log returns
+    rf_annual=sum(rf_reshape,1)'; % Get annual log returns
+
+    Rm_annual=exp(rm_annual)-1;
+    Rf_annual=exp(rf_annual)-1;
+
+    excess_annual=Rm_annual-Rf_annual;
+
+    PD_level=exp(zm_sim(t_years));
+    PD_reshape=reshape(PD_level,12,years);
+    PD_December=PD_reshape(12,:)'; % Keep one observation per year
+
+    PD_annual=zeros(years,1);
+
+    for i=1:years
+        gd_annual=gd_reshape(:,i);
+        growth_annual=exp(sum(gd_annual)); % Gross dividend growth over a year
+        sum_dividends = sum(exp(cumsum(gd_annual))); % Sum of dividend levels divided by the initial level
+        PD_annual(i)=PD_December(i)*growth_annual/sum_dividends; % Price divided by sum of dividends
+    end
+
+    pd_annual=log(PD_annual);
+
+    expected_excess=mean(excess_annual)*100;
+    expected_Rf=mean(Rf_annual)*100;
+    sigma_Rm=std(Rm_annual)*100;
+    sigma_Rf=std(Rf_annual)*100;
+
+    sigma_pd=std(pd_annual);
+    expected_pd=mean(pd_annual);
+end
+
+T=100000;
+burn_pop=5000;
+rng(seed);
+
+[expected_excess,expected_rf,sigma_rm,sigma_rf,sigma_pd,expected_pd] = function2_simulate_vfi_tauchen_table2(...
+    T,burn_pop,rho,phi_e,sigma,mu,mu_d,phi,phi_d,beta,psi,theta,x_grid,V1_PC,V1_PD,P_matrix,nu1,sigma_w,sigma2_grid,S_matrix,Nx,Ns2);
+
+fprintf('TABLE V MOMENTS (VFI (TAUCHEN) SIMULATION: POPULATION)\n');
+fprintf('E(Rm-Rf)=%9.3f\n',expected_excess);
+fprintf('E(Rf)= %11.3f\n',expected_rf);
+fprintf('sigma(Rm)=%8.3f\n',sigma_rm);
+fprintf('sigma(Rf)=%8.3f\n',sigma_rf);
+fprintf('sigma(p-d)=%7.3f\n',sigma_pd);
+fprintf('E(exp(p-d))=%7.3f\n',expected_pd);
+
+% MONTE CARLO
+num_sim=500;
+Tmonths=840;
+burn_mc=100;
+
+expected_excess_mc=zeros(num_sim,1);
+expected_Rf_mc=zeros(num_sim,1);
+sigma_Rm_mc=zeros(num_sim,1);
+sigma_Rf_mc=zeros(num_sim,1);
+sigma_pd_mc=zeros(num_sim,1);
+expected_pd_mc=zeros(num_sim,1);
+
+for k=1:num_sim
+    [expected_excess,expected_Rf,sigma_Rm,sigma_Rf,sigma_pd,expected_pd]= ...
+        function2_simulate_vfi_tauchen_table2( ...
+        Tmonths+burn_mc,burn_mc,rho,phi_e,sigma,mu,mu_d,phi,phi_d,beta,psi,theta,x_grid,V1_PC,V1_PD,P_matrix,nu1,sigma_w,sigma2_grid,S_matrix,Nx,Ns2);
+
+    expected_excess_mc(k)=expected_excess;
+    expected_Rf_mc(k)=expected_Rf;
+    sigma_Rm_mc(k)=sigma_Rm;
+    sigma_Rf_mc(k)=sigma_Rf;
+    sigma_pd_mc(k)=sigma_pd;
+    expected_pd_mc(k)=expected_pd;
+end
+
+mean_expected_excess_mc=mean(expected_excess_mc);
+mean_expected_rf_mc=mean(expected_Rf_mc);
+mean_sigma_rm_mc=mean(sigma_Rm_mc);
+mean_sigma_rf_mc=mean(sigma_Rf_mc);
+mean_sigma_pd_mc=mean(sigma_pd_mc);
+mean_expected_pd_mc=mean(expected_pd_mc);
+
+fprintf('TABLE V MOMENTS (VFI (TAUCHEN) SIMULATION: MONTE CARLO)\n');
+fprintf('E(Rm-Rf)=%10.3f\n',mean_expected_excess_mc);
+fprintf('E(Rf)= %12.3f\n',mean_expected_rf_mc);
+fprintf('sigma(Rm)=%9.3f\n',mean_sigma_rm_mc);
+fprintf('sigma(Rf)=%9.3f\n',mean_sigma_rf_mc);
+fprintf('sigma(p-d)=%8.3f\n',mean_sigma_pd_mc);
+fprintf('E(exp(p-d))=%7.3f\n',mean_expected_pd_mc);
